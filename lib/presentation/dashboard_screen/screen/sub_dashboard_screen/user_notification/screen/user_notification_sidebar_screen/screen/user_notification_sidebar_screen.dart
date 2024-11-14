7aedc7fca3db4e6fbe283176a4249db8e89bea33
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-11-14 19:42:52 +0530

    user notification sidebar

--- lib/presentation/dashboard_screen/screen/sub_dashboard_screen/user_notification/screen/user_notification_sidebar_screen/screen/user_notification_sidebar_screen.dart
+++ lib/presentation/dashboard_screen/screen/sub_dashboard_screen/user_notification/screen/user_notification_sidebar_screen/screen/user_notification_sidebar_screen.dart
@@ -51,105 +51,23 @@ class UserNotificationSidebarScreen extends GetWidget {
                     child: ListView(
                       children: [
                         DropDownOptions(
+                          icon: Icons.notifications_outlined,
+                          title: "Notifications",
+                          items: const [],
+                          scale: scale,
+                        ),
+                        DropDownOptions(
                           icon: Icons.description_outlined,
-                          title: "Overview",
+                          title: "Reports",
                           items: const [],
                           scale: scale,
                         ),
                         DropDownOptions(
                           icon: Icons.warning_amber_rounded,
-                          title: "Security Alerts",
+                          title: "Real Time Alerts",
                           items: const [],
                           scale: scale,
                         ),
-                        DropDownOptions(
-                          icon: Icons.book_outlined,
-                          title: "Legal",
-                          items: const [
-                            "Copyright Policy",
-                            "Terms & Conditions",
-                            "Data Provider Attribution"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.settings_outlined,
-                          title: "General Settings",
-                          items: const [
-                            "Language Preference",
-                            "Arabic",
-                            "English"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.person_pin_outlined,
-                          title: "Account Information",
-                          items: const [
-                            "Email address",
-                            "Username",
-                            "Profile picture"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.dataset_outlined,
-                          title: "Privacy Settings",
-                          items: const [],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.candlestick_chart_outlined,
-                          title: "Security Settings",
-                          items: const [
-                            "Password Management",
-                            "Biometric Security",
-                            "Device Management"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.cloud_queue_outlined,
-                          title: "Data Management",
-                          items: const [
-                            "Backup & Sync",
-                            "Data Usage",
-                            "Clear Cache/Data"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.person_pin_outlined,
-                          title: "Account Management",
-                          items: const [
-                            "Subscription Information",
-                            "Payment Methods"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.message_outlined,
-                          title: "Support & Feedback",
-                          items: const [
-                            "Help & Support",
-                            "Send Feedback",
-                            "App Version Info"
-                          ],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.logout_outlined,
-                          title: "Logout",
-                          items: const [],
-                          scale: scale,
-                        ),
-                        DropDownOptions(
-                          icon: Icons.copyright_outlined,
-                          title: "Version History",
-                          items: const [],
-                          scale: scale,
-                        ),
-                        SizedBox(height: scale.getScaledHeight(120)),
                       ],
                     ),
                   ),
